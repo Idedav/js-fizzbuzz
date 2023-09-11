7aaf35440977f19c47130f9aba44dd2796a59ada
Idedav <xdavide.correa@gmail.com>
2023-09-11 14:33:29 +0200

    [ADD] content on html and [ADD] syle on css and [ADD] log js on dom

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
+const boxesContainer = document.querySelector('.boxes')
+
 for(let i = 0; i < 100; i++){
-
-    if(!((i + 1) % 3)){
-        console.log('Fizz')
-    }else if(!((i + 1) % 5)){
-        console.log('Buzz')
-    }
     
-    if(!((i + 1 ) % 3) && !((i + 1) % 5)){
-        console.log('FizzBuzz')
+    const box = document.createElement('div');
+    box.classList.add('box');
+
+    if(!((i + 1) % 3) && !((i + 1) % 5)){
+        box.classList.add('fizz-buzz')
+        box.append('FizzBuzz')
+    }else if(!((i + 1) % 3)){
+        box.classList.add('fizz')
+        box.append('Fizz')
+    }else if(!((i + 1) % 5)){
+        box.classList.add('buzz')
+        box.append('Buzz')
+    }else{
+        box.append(i + 1)
     }
 
-    console.log(i + 1)
+    boxesContainer.append(box)
 
 }
